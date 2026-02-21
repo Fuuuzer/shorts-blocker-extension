0e91b4a65b876519ac365b8b4a84e8bd1afdcafe
Joao Fuzer <fuuzer_@hotmail.com>
2026-02-21 16:06:28 -0300

    fix a lot observations on script

--- content.js
+++ content.js
@@ -8,10 +8,13 @@ checkIfUrlIsShorts();
 
 const callback = () => {
   if (lastUrl !== location.href) {
+    console.log(location.href);
+    checkIfUrlIsShorts();
+    if (location.pathname.startsWith("/shorts")) {
+      console.log("você está na aba shorts");
+    }
     lastUrl = location.href;
-    console.log("URL mudou");
   }
-  checkIfUrlIsShorts();
 };
 const observer = new MutationObserver(callback);
 observer.observe(document.body, {
