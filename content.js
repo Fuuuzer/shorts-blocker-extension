7b7b2807762bb6386eaea69fb56497d8a198ffe9
Joao Fuzer <fuuzer_@hotmail.com>
2026-02-21 17:01:21 -0300

    add pause short video

--- content.js
+++ content.js
@@ -1,23 +1,58 @@
 // console.log(location.href);
-let lastUrl = location.href;
+let lastPath = location.pathname;
+
+function createWarningOverlay() {
+  const overlay = document.createElement("div");
+  overlay.style.position = "fixed";
+  overlay.style.zIndex = "999999";
+  overlay.style.width = "100%";
+  // overlay.style.height = "100%";
+  overlay.textContent = "Você quer entrar no loop?";
+  overlay.style.backgroundColor = "red";
+  document.body.style.overflow = "hidden";
+  console.log("overlay criado");
+  document.body.appendChild(overlay);
+}
+
+function pauseVideos() {
+  const videos = document.querySelectorAll("video");
+
+  const intervalPauseVideos = setInterval(() => {
+    videos.forEach((video) => {
+      video.pause();
+      console.log("video pausado");
+      if (video.paused) {
+        clearInterval(intervalPauseVideos);
+      }
+    });
+  }, 500);
+  createWarningOverlay();
+}
 
 function checkIfUrlIsShorts() {
-  console.log("verificando URL: ", location.href);
+  if (location.pathname.startsWith("/shorts")) {
+    console.log("você está na aba shorts");
+    pauseVideos();
+  }
 }
 checkIfUrlIsShorts();
 
-const callback = () => {
-  if (lastUrl !== location.href) {
-    console.log(location.href);
+document.addEventListener("yt-navigate-finish", () => {
+  if (lastPath !== location.pathname) {
+    lastPath = location.pathname;
+    console.log("Mudou o path");
     checkIfUrlIsShorts();
-    if (location.pathname.startsWith("/shorts")) {
-      console.log("você está na aba shorts");
-    }
-    lastUrl = location.href;
   }
-};
-const observer = new MutationObserver(callback);
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
 });
+
+// const callback = () => {
+//   if (lastPath !== location.pathname) {
+//     lastPath = location.href;
+//     checkIfUrlIsShorts();
+//   }
+// };
+// const observer = new MutationObserver(callback);
+// observer.observe(document.body, {
+//   childList: true,
+//   subtree: true,
+// });
