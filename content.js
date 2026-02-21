c363ea3446af5fc6fa0329bca0b7f16e076d21c2
Joao Fuzer <fuuzer_@hotmail.com>
2026-02-21 18:16:56 -0300

    made mutation observer, if user exit /shorts and comeback the observer starts again[

--- content.js
+++ content.js
@@ -1,16 +1,23 @@
 // console.log(location.href);
 let lastPath = location.pathname;
+let observer = null;
+let observerActive = false;
+const overlay = document.createElement("div");
+
+function removeOverlay() {
+  if (overlay) overlay.remove();
+}
 
 function createWarningOverlay() {
   if (!document.getElementById("shorts-blocker-overlay")) {
-    const overlay = document.createElement("div");
     overlay.style.position = "fixed";
     overlay.id = "shorts-blocker-overlay";
     overlay.style.zIndex = "999999";
-    overlay.style.width = "100vw";
-    overlay.style.height = "100vh";
+    overlay.style.width = "50vw";
+    overlay.style.height = "50vh";
     overlay.textContent = "Para de assistir shorts e vá fazer algo da vida";
     overlay.style.fontSize = "5rem";
+    overlay.style.color = "white";
     overlay.style.backgroundColor = "red";
     document.body.style.overflow = "hidden";
     // console.log("overlay criado");
@@ -18,18 +25,19 @@ function createWarningOverlay() {
   }
 }
 
+function muteVideos() {
+  const videos = document.querySelectorAll("video");
+  videos.forEach((video) => {
+    video.muted = true;
+  });
+}
+
 function pauseVideos() {
   const videos = document.querySelectorAll("video");
-
-  const intervalPauseVideos = setInterval(() => {
-    videos.forEach((video) => {
-      video.pause();
-      // console.log("video pausado");
-      if (video.paused) {
-        clearInterval(intervalPauseVideos);
-      }
-    });
-  }, 500);
+  videos.forEach((video) => {
+    video.pause();
+    // console.log("video pausado");
+  });
   createWarningOverlay();
 }
 
@@ -37,6 +45,13 @@ function checkIfUrlIsShorts() {
   if (location.pathname.startsWith("/shorts")) {
     // console.log("você está na aba shorts");
     pauseVideos();
+    startObserver();
+  } else {
+    if (observer) {
+      observer.disconnect();
+      observerActive = false;
+      removeOverlay();
+    }
   }
 }
 checkIfUrlIsShorts();
@@ -49,14 +64,26 @@ document.addEventListener("yt-navigate-finish", () => {
   }
 });
 
-// const callback = () => {
-//   if (lastPath !== location.pathname) {
-//     lastPath = location.href;
-//     checkIfUrlIsShorts();
-//   }
-// };
-// const observer = new MutationObserver(callback);
-// observer.observe(document.body, {
-//   childList: true,
-//   subtree: true,
-// });
+function startObserver() {
+  if (!observer) {
+    observer = new MutationObserver(() => {
+      if (location.pathname.startsWith("/shorts")) {
+        createWarningOverlay();
+        pauseVideos();
+      }
+    });
+    observerActive = true;
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  if (observer && observerActive === false) {
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+    observerActive = true;
+  }
+}
